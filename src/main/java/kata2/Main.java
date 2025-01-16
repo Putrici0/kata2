0f302952cfdc897d8c8b6b8096b873fd4f75699d
paull <paullopisparrilla@gmail.com>
2025-01-16 10:40:24 +0000

    Code details refactored on main.

--- src/main/java/kata2/Main.java
+++ src/main/java/kata2/Main.java
@@ -12,7 +12,7 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        List<Title> titleList = TsvTitleLoader.with(new File("C:/Users/paull/Desktop/Ingenieria Informática - ULPGC/Tercero/is2 katas/kata2/title.basics.tsv")).load();
+        List<Title> titleList = TsvTitleLoader.with(new File("title.basics.tsv")).load();
 
         Map<String, Integer> statistics = new TotalTitleStatistics().execute(titleList);
         for (String key : statistics.keySet()) {
